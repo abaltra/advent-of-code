656a355eb31a729bc88baab5067d069a573119ac
Alejandro Baltra <abaltra@billtrust.com>
2022-12-06 12:07:10 -0500

    Day 6 refactoring

--- 2022/day6/entry.go
+++ 2022/day6/entry.go
@@ -1,24 +1,74 @@
-	package day6
+package day6
 
-	import (
-		"bufio"
-		"fmt"
-		"os"
-	)
+import (
+	"bufio"
+	"errors"
+	"fmt"
+	"os"
+)
 
-	func Run() {
-  	println("hello 6")
+type LimitQueue struct {
+	values []byte
+	limit int
+}
+
+func NewQueue (limit int) LimitQueue {
+	return LimitQueue {
+		limit: limit,
+		values: make([]byte, 0),
+	}
+}
+
+func (q *LimitQueue) Push (v byte) {
+	q.values = append(q.values, v)
+	if len(q.values) > q.limit {
+		q.values = q.values[1:]
+	}
+}
+
+func (q *LimitQueue) Pop () (byte, error) {
+	if len(q.values) == 0 {
+		return byte(0), errors.New("empty queue")
+	}
+
+	v := q.values[0]
+	q.values = q.values[1:]
+
+	return v, nil
+}
+
+func (q LimitQueue) IsSizedSet() bool {
+	if len(q.values) != q.limit {
+		return false
+	}
+
+	collisions := make(map[byte]int)
+
+	for _, value := range q.values {
+		collisions[value] ++
+		if collisions[value] > 1 {
+			return false
+		}
+	}
+
+	return true
+}
+
+func Run() {
+  println("hello 6")
 
   	
-  	input, err := os.Open("./day6/input.txt")
+  input, err := os.Open("./day6/input.txt")
 
-		if err != nil {
+	if err != nil {
 		panic(err)
 	}
 
 	filescanner := bufio.NewScanner(input)
 	filescanner.Split(bufio.ScanBytes)
 
+	q := NewQueue(4)
+	qMessage := NewQueue(14)
 
 	queue := make([]byte, 0)
 	messageQueue := make([]byte, 0)
@@ -33,6 +83,9 @@
 		queue = append(queue, c[0])
 		messageQueue = append(messageQueue, c[0])
 
+		q.Push(c[0])
+		qMessage.Push(c[0])
+
 		if len(queue) > 4 {
 			queue = queue[1:]
 		}
@@ -41,18 +94,14 @@
 			messageQueue = messageQueue[1:]
 		}
 
-		if len(queue) == 4 && sopIndex == -1 {
-			if areUnique(queue) {
-				fmt.Printf("Found SOP marker: %s. Data starts at index %d\n", string(queue), counter + 1)
-				sopIndex = counter + 1
-			}
+		if q.IsSizedSet() && sopIndex == -1 {
+			fmt.Printf("Found SOP marker: %s. Data starts at index %d\n", string(q.values), counter + 1)
+			sopIndex = counter + 1
 		}
 
-		if len(messageQueue) == 14 && somIndex == -1 {
-			if areUnique(messageQueue) {
-				fmt.Printf("Found SOM marker: %s. Data starts at index %d\n", string(messageQueue), counter + 1)
-				somIndex = counter + 1
-			}
+		if qMessage.IsSizedSet() && somIndex == -1 {
+			fmt.Printf("Found SOM marker: %s. Data starts at index %d\n", string(qMessage.values), counter + 1)
+			somIndex = counter + 1
 		}
   	counter ++
   }
