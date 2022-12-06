77ea829d54523018e7c296c399a5f8ba638d7ad6
Alejandro Baltra <abaltra@billtrust.com>
2022-12-06 10:32:34 -0500

    day 6

--- 2022/main.go
+++ 2022/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/abaltra/aoc2022/day3"
 	"github.com/abaltra/aoc2022/day4"
 	"github.com/abaltra/aoc2022/day5"
+	"github.com/abaltra/aoc2022/day6"
 )
 
 func main() {
@@ -14,4 +15,5 @@ func main() {
 	day3.Run()
 	day4.Run()
 	day5.Run()
+	day6.Run()
 }
